cb47b178da18c481dd968dd3b794425e1878c27e
Matheus Falcão <theusx@gmail.com>
2017-06-01 23:41:03 -0300

    :wrench: Change the version by package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var twig = require('gulp-twig');
+const version = require('./package.json').version
 
 gulp.task('compile', function () {
     return gulp.src(['./src/**/*.twig', '!./src/base.twig', '!./src/macros.twig', '!./src/**/_*.twig'])
-        .pipe(twig())
+        .pipe(twig({
+            data: {
+              version: version
+            }
+          }))
         .pipe(gulp.dest('./'));
 });
 
